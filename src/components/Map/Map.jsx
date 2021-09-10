3852202a950896dc99f382aacc04d9a33bc021cf
Brandon Wright <thewrightbrandon@gmail.com>
2021-09-09 22:43:25 -0400

    Made call to Travel Advisor API, implemented a few hooks to grab data to fill state

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab'
+import Rating from '@material-ui/lab';
 
-import useStyles from './styles'
+import useStyles from './styles';
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
 
   const classes = useStyles()
   const onMobile = useMediaQuery('(min-width:600px)')
-  const coordinates = { lat: 40.7128, lng: -74.0060 }
 
   return (
     <div className={classes.mapContainer}>
@@ -21,7 +20,17 @@ const Map = () => {
         defaultZoom={12}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
+        onChange={(event) => {
+          console.log(event);
+          setCoordinates({
+            lat: event.center.lat,
+            lng: event.center.lng
+          })
+          setBounds({
+            ne: event.marginBounds.ne,
+            sw: event.marginBounds.sw
+          })
+        }}
         onChildClick={''}
       >
 
